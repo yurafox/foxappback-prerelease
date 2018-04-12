6bf81032e72ea673551bb8d670e4fde0b18e235d
vargav <dealio07@gmail.com>
2018-04-12 17:34:55 +0300

    stores where command changed in startup.cs

--- Wsds.WebApp/Startup.cs
+++ Wsds.WebApp/Startup.cs
@@ -247,7 +247,7 @@ namespace Wsds.WebApp
                 new EntityConfig(mainDataConnString)
                     .AddSqlCommandSelect("select t.id, JSON_OBJECT('id' value id, 'idCity' value id_city, 'address' value address_line, 'lat' value lat,'lng' value lng," +
                                          "'openTime' value open_time, 'closeTime' value close_time, 'rating' value rating, 'idFeedbacks' value id_feedbacks) as value from STORE_PLACES t ")
-                    .AddSqlCommandWhere("where t.type=1  and t.lat is not null  and t.lng is not null  and t.is_active=1 and t.open_time is not null and t.close_time is not null")
+                    .AddSqlCommandWhere("where t.type=1  and t.lat is not null  and t.lng is not null  and t.is_active=1")
                     .SetKeyField("id")
                     .SetValueField("value")
                     .SetSerializerFunc("Serialization.Store2Json")
